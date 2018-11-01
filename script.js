5cab6b5876a2e3bb721da30a12479af50bb0f7fd
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-11-01 17:57:57 -0300

    5th commit: Adequação ao escopo (Layout e outras alterações)

--- script.js
+++ script.js
@@ -1,62 +1,73 @@
 var pontos = 0;
-var autoclique = 0;
+var autoclique = 1;
+autoclique3 = 0;
 var upgrades = [false, false, false];
+var segundos = 0;  var minutos = 0;
 
 function inicioJogo(){
   setInterval(printaPontos, 100);
-  document.getElementById('homer').src = "img/homer_001.jpg"
+  setInterval(cronometro, 1000);
+  setInterval(upgradeum, 100);
 }
 
 function printaPontos() {
-  document.getElementById("pontos").innerHTML = "Seus Donuts: "+pontos;
-  if (pontos >= 20 && upgrades[0] === false) {
-      document.getElementById('upgrade1').src = "img/donuts_upgrade_001.jpg"
-  }else{
-    document.getElementById('upgrade1').src = "img/donuts_upgrade_000.jpg"}
-  if(pontos >= 250 && upgrades[1] === false){
-  document.getElementById('upgrade2').src = "img/donuts_upgrade_002.jpg"
-}else {
-  document.getElementById('upgrade2').src = "img/donuts_upgrade_000.jpg"
+    document.getElementById("pontos").innerHTML = "Seus Donuts: "+pontos;
+    if (pontos >= 20 && upgrades[0] === false) {
+    document.getElementById('upgrade1').src = "img/donuts_upgrade_001.jpg"
 }
-  if(pontos >= 500 && upgrades[2] === false){
+  if(pontos >= 50 && upgrades[1] === false){
+  document.getElementById('upgrade2').src = "img/donuts_upgrade_002.jpg"
+}
+  if(pontos >= 70 && upgrades[2] === false){
   document.getElementById('upgrade3').src = "img/donuts_upgrade_003.jpg"
-}else {
-  document.getElementById('upgrade3').src = "img/donuts_upgrade_000.jpg"
 }
 }
 function placarPontos(){
     document.getElementById("pontos").innerHTML = "Seus Donuts: "+pontos;
     pontos += 1;
-    document.getElementById('homer').src = "img/homer_000.jpg"
+    document.getElementById('homer').src = "img/homer_000.jpg";
+    setTimeout(function(){  document.getElementById('homer').src = "img/homer_001.jpg"; }, 100);
+    var som = document.getElementById("somHomer");
+    som.play();
 }
+
+function upgradeTempo3(){
+  pontos += autoclique3;
+}
+
 function upgradeTempo(){
 	pontos += autoclique;
   document.getElementById("pontos").innerHTML = "Seus Donuts: "+pontos;
 }
-  function upgradeum(){
+
+function upgradeum(){
     if (pontos >= 20 && upgrades[0] === false){
       upgrades[0] = true;
-   		pontos -= 20;
-   		autoclique += 1;
       setInterval (upgradeTempo, 20000);
-      document.getElementById('upgrade1').src = "img/donuts_upgrade_000.jpg"
-   	}
-   }
-function upgradedois(){
-  if(pontos >= 250 && upgrades[1] === false){
-    upgrades[1] = true;
-    pontos -= 250;
-    autoclique += 1;
-    setInterval (upgradeTempo, 1000);
-    document.getElementById('upgrade2').src = "img/donuts_upgrade_000.jpg"
+   	}else if(pontos >= 50 && upgrades[1] === false) {
+      upgrades[1] = true;
+      setInterval (upgradeTempo, 1000);
+   }else if (pontos >= 70 && upgrades[2] === false) {
+     upgrades[2] = true;
+     autoclique3 += 5;
+     setInterval (upgradeTempo3, 10000);
   }
 }
-function upgradetres() {
-  if (pontos >= 500 && upgrades[2] === false) {
-    upgrades[2] = true;
-    pontos -= 500;
-    autoclique += 5;
-    setInterval (upgradeTempo, 10000);
-    document.getElementById('upgrade3').src = "img/donuts_upgrade_000.jpg"
-  }
+
+function cronometro() {
+    segundos += 1;
+    if (segundos >= 60) {
+      segundos = 0;
+      minutos++;
+}
+    if (minutos >=60) {
+      minutos = 0;
+    }
+    if (segundos < 10) {
+      document.getElementById("tempo").innerHTML = "0"+minutos+":0"+segundos;
+}else if(minutos < 10){
+      document.getElementById("tempo").innerHTML = "0"+minutos+":"+segundos;
+}else {
+      document.getElementById("tempo").innerHTML = minutos+":"+segundos;
+}
 }
